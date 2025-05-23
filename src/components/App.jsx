de04eee010af0658c9fd10030c94d58c6c660df4
Олексій Демидов <agdemidof@gmail.com>
2025-05-23 15:44:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,11 +47,11 @@ useEffect(() => {
       <Router>
        
         <div className={s.container}>
-           {/* <Header /> */}
+           <Header />
           <CartProvider>
             <Routes>
               <Route path="/" element={<Layout />}>
-                {/* <Route index element={<Main menuData={menuData} />} /> */}
+                <Route index element={<Main menuData={menuData} />} />
                 <Route path="admin" element={<Admin />} />
                 <Route path="auth/login" element={<Login />} />
                 <Route path="auth/register" element={<Register />} />
